79e72ced1024e8e4a83f7101ddcadfeb15865675
Muzaki-0 <80158508+Muzaki-0@users.noreply.github.com>
2026-09-20 00:37:58 +0800

    Create Kalkulatormeh.java

---
+++ Kalkulatormeh.java
@@ -0,0 +1,11 @@
+public class Kalkulatormeh {
+  public static void main(String [] args){
+    int angka1,angka2;
+    angka1 = 20;
+    angka2 = 30;
+    System.out.println("(+) hasil tambah:" + (angka1 + angka2));
+    System.out.println("(-) hasil kurang:" + (angka1 - angka2));
+    System.out.println("(*) hasil kali:" + (angka1 * angka2));
+    System.out.println("(/) hasil bagi:" + (angka1 / angka2));
+  }
+}
